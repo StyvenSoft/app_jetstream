14f945f7d9154192b263ba287416c07bac1d208e
Steveen <steveen.echeverri@gmail.com>
2021-03-22 17:04:26 -0500

    Basic route management completed Get

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,40 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+// Retornando text de prueba
+Route::get('/text', function () {
+    return 'Welcome text!!';
+});
+
+// Retornando array
+Route::get('/array', function () {
+    $days = ['Monday', 'Tuesday', 'Webnesday', 'Thursday', 'Friday'];
+    return $days;
+});
+
+// Retornando Parametros
+Route::get('/name/{name}', function ($name) {
+    return '<h1>My name is: '.$name.'</h1>';
+});
+
+// Retorando Parametros por defecto
+Route::get('/client/{client}/', function ($client='General CLient') {
+    return '<h1>Welcome: '.$client.'</h1>';
+});
+
+// Redireccionamiento de rutas
+
+Route::get('/route1', function () {
+    return 'View route 1';
+})->name('RouteOne');
+
+Route::get('/route2', function () {
+    return redirect()->route('RouteOne');
+});
+
+// Validación de parametros [A-Za-z]
+
+Route::get('/user/{id}', function ($id) {
+    return 'The user id is: '. $id;
+})->where('id', '[0-9]+');
